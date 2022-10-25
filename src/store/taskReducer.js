2ce0e1cf8fa3392383544d008b7558c2ea9d540f
Eugene015 <egavriluk@gmail.com>
2022-10-25 10:03:54 +0200

    1.0.0

---
+++ src/store/taskReducer.js
@@ -0,0 +1,25 @@
+import { taskUpdated, taskDeleted } from "./actionTypes";
+
+export function taskReducer(state = [], action) {
+  switch (action.type) {
+    case taskUpdated: {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex] = {
+        ...newArray[elementIndex],
+        ...action.payload,
+      };
+      return newArray;
+    }
+    case taskDeleted: {
+      let newArray = [...state];
+      const filtered = newArray.filter((el) => el.id !== action.payload.id);
+      newArray = [...filtered];
+      return newArray;
+    }
+    default:
+      return state;
+  }
+}
